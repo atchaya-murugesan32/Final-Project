1bece3099d2661628dbcc829a35291bb982d55fd
Anngeni Vincent <anngenivincent@gmail.com>
2026-06-21 17:27:34 +0100

    updated CafeResponse to have more fields

--- CAFEMONITOR/app/models/response_models.py
+++ CAFEMONITOR/app/models/response_models.py
@@ -1,8 +1,15 @@
+
 from pydantic import BaseModel
 
 class CafeResponse(BaseModel):
+    id: str
     name: str
     address: str
     rating: float
+    rating_count: int
     busyness: str
-    price_level: str
+    price_range: str
+    maps_uri: str
+    website_uri: str | None = None
+    photos: list[str] = []
+    opening_hours: list[str] = []
